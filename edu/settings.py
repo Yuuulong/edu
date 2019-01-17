17ccf9f45cf8a22e590e1c7e5dec7f92ede38a41
Yuuulong <yuuuulong@gmail.com>
2019-01-17 16:01:41 +0800

    setting静态文件目录设置

--- edu/settings.py
+++ edu/settings.py
@@ -162,4 +162,4 @@ EMAIL_USE_SSL = True  # 与SMTP服务器通信时，是否启动SSL链接(安全
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # 上传资源文件配置
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # 上传资源文件配置,收集静态文件配置
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # 上传资源文件配置,收集静态文件配置
