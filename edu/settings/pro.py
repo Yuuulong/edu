d88f2ffd3a82b7c8455d7f66d3ea3714a09b7089
Yuuulong <yuuuulong@gmail.com>
2019-01-19 21:24:34 +0800

    pro环境配置

--- edu/settings/pro.py
+++ edu/settings/pro.py
@@ -8,13 +8,13 @@ ADMINS = (('Antonio M', 'yuuuulong@gmai.com'), )
 
 ALLOWED_HOSTS = ['*']
 
-DATABASES_PASSWORD = os.environ['DATABASES_PASSWORD']  # 将数据库密码保存在本地环境
+DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']  # 将数据库密码保存在本地环境
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'edu_db',
         'USER': 'root',
-        'PASSWORD': DATABASES_PASSWORD,
+        'PASSWORD': DATABASE_PASSWORD,
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
@@ -31,31 +31,31 @@ EMAIL_USE_SSL = True  # 与SMTP服务器通信时，是否启动SSL链接(安全
 
 ADMINS = (('admin', '329227939@qq.com'), )
 
-# 日志文件
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/home/edu_debug.log',
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler',
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-    },
-}
+# # 日志文件
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'file': {
+#             'level': 'DEBUG',
+#             'class': 'logging.FileHandler',
+#             'filename': '/home/edu_debug.log',
+#         },
+#         'mail_admins': {
+#             'level': 'ERROR',
+#             'class': 'django.utils.log.AdminEmailHandler',
+#         }
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['file'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#         'django.request': {
+#             'handlers': ['mail_admins'],
+#             'level': 'ERROR',
+#             'propagate': False,
+#         },
+#     },
+# }
